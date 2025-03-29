6249b6d5f89a286cc9c2ef1d9183ae5335f4c552
tiyn <tiyn@posteo.eu>
2025-03-29 06:24:56 +0100

    fixed deprecated function

--- lua/docs-view.lua
+++ lua/docs-view.lua
@@ -26,8 +26,8 @@ M.update = function()
     position = { line = l - 1, character = c },
   }, function(err, result, ctx, config)
     if win and vim.api.nvim_win_is_valid(win) and result and result.contents then
-      print(result.contents)
-      local md_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+      print(table.concat(result.contents,"\n"))
+      local md_lines = vim.lsp.util.convert_input_to_markdown_lines(table.concat(result.contents,"\n"))
       -- print(md_lines)
       md_lines = vim.gsplit(md_lines, "\n", { trimempty = true })
       if vim.tbl_isempty(md_lines) then
